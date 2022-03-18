43ae585ecea50647515aede16b5781cf74d869f1
hfduran <henriqueduran15@gmail.com>
2022-03-17 21:07:14 -0300

    add: componente piu / 2

--- src/components/WriteArea/styles.ts
+++ src/components/WriteArea/styles.ts
@@ -1,2 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+
+export const WABody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* width: 100%; */
+  border-bottom: 2px solid #696868;
+  padding: 8px;
+  padding-bottom: 24px;
+  margin-bottom: 24px;
+`;
+
+export const WAContent = styled.div``;
