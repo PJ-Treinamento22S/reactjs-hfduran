13ba5ef67415f01e706a1f5d7a0c09115ab0777d
hfduran <henriqueduran15@gmail.com>
2022-03-18 19:22:09 -0300

    feat: contador de palavras

--- src/components/WriteArea/index.tsx
+++ src/components/WriteArea/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
 
 import ProfilePic from "../../images/avatar.jpg";
 
 const WriteArea: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
       <S.WABody>
         <S.WAInputAndImg>
           <S.WAImage src={ProfilePic}></S.WAImage>
-          <S.WAInput placeholder="No que você está pensando?"></S.WAInput>
+          <S.WAInput
+            placeholder="No que você está pensando?"
+            onChange={(e) => setCounter(e.target.value.length)}
+          ></S.WAInput>
         </S.WAInputAndImg>
         <S.WASubmit>
-          <S.WASubmitCounter>0/140</S.WASubmitCounter>
+          <S.WASubmitCounter>{counter}/140</S.WASubmitCounter>
           <S.WASubmitButton>Piar!</S.WASubmitButton>
         </S.WASubmit>
       </S.WABody>
